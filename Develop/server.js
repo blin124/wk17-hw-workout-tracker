6f2e58cf75fbb634688e49e5ea34209b0fd6f732
Bao <lbx01072004@gmail.com>
2020-06-17 19:26:07 +1000

    add all files along with new server.js file

---
+++ Develop/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const User = require("./api.js");
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.get("/exercise", ({body}, res) => {
+    
+})
+app.get("/workouts", ({body}, res) => {
+    
+})
+app.get("/workouts/range", ({body}, res) => {
+    
+})
+
+app.post("/submit", ({body}, res) => {
+  User.create(body)
+    .then(dbUser => {
+      res.json(dbUser);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
